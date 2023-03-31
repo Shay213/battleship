a409a6c38cbc3045ea2e8bfa1a8a2f643a996f63
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-03-31 16:45:30 +0200

    Create copy of original board for testing purpses

--- src/__tests__/gameboard.test.js
+++ src/__tests__/gameboard.test.js
@@ -4,22 +4,22 @@ const gameBoard = GameBoard();
 
 describe('placing ships on the board', () => {
 
+    const board = gameBoard.getBoard(); // get a deep copy of the board
+
     test('Mark square on board as taken based on received cords', () => {
-        gameBoard.placeShip([7,7]);
-        const board = gameBoard.getBoard();
+        gameBoard.placeShip([7,7], {testBoard: board});
         expect(board[7][7]).toBe(1);
     });
 
 
     test('check if received square is valid or taken', () => {
-        expect(() => gameBoard.placeShip([7, 7])).toThrow();
-        expect(() => gameBoard.placeShip([10, 10])).toThrow();
+        expect(() => gameBoard.placeShip([7, 7], {testBoard: board})).toThrow();
+        expect(() => gameBoard.placeShip([10, 10], {testBoard: board})).toThrow();
     });
 
     describe('testing horizontal ships', () => {
         test('mark multiple squares if ship length > 1', () => {
-            gameBoard.placeShip([0, 0], {length: 4});
-            const board = gameBoard.getBoard();
+            gameBoard.placeShip([0, 0], {length: 4, testBoard: board});
             expect(board[0][0]).toBe(1);
             expect(board[0][1]).toBe(1);
             expect(board[0][2]).toBe(1);
@@ -27,22 +27,20 @@ describe('placing ships on the board', () => {
         });
         
         test('check if ship with length > 1 will fit', () => {
-            expect(() => gameBoard.placeShip([0, 7], {length: 4})).toThrow();
+            expect(() => gameBoard.placeShip([0, 7], {length: 4, testBoard: board})).toThrow();
         });
         
         test('ships separated by 1 square', () => {
             const length = 3;
-            expect(() => gameBoard.placeShip([0, 4], {length: length})).toThrow();
-            expect(() => gameBoard.placeShip([1, 4], {length: length})).toThrow();
-            expect(() => gameBoard.placeShip([1, 0], {length: length})).toThrow();
+            expect(() => gameBoard.placeShip([0, 4], {length: length, testBoard: board})).toThrow();
+            expect(() => gameBoard.placeShip([1, 4], {length: length, testBoard: board})).toThrow();
+            expect(() => gameBoard.placeShip([1, 0], {length: length, testBoard: board})).toThrow();
         });
     });
 
     describe('testing vertical ships', () => {
         test('mark multiple squares if ship length > 1', () => {
-            console.log(gameBoard.getBoard());
-            gameBoard.placeShip([0, 5], {length: 4, vertical: true});
-            const board = gameBoard.getBoard();
+            gameBoard.placeShip([0, 5], {length: 4, vertical: true, testBoard: board});
             expect(board[0][5]).toBe(1);
             expect(board[1][5]).toBe(1);
             expect(board[2][5]).toBe(1);
@@ -50,14 +48,14 @@ describe('placing ships on the board', () => {
         });
         
         test('check if ship with length > 1 will fit', () => {
-            expect(() => gameBoard.placeShip([7, 0], {length: 4, vertical: true})).toThrow();
+            expect(() => gameBoard.placeShip([7, 0], {length: 4, vertical: true, testBoard: board})).toThrow();
         });
         
         test('ships separated by 1 square', () => {
             const length = 3;
-            expect(() => gameBoard.placeShip([0, 6], {length: length, vertical: true})).toThrow();
-            expect(() => gameBoard.placeShip([1, 4], {length: length, vertical: true})).toThrow();
-            expect(() => gameBoard.placeShip([4, 4], {length: length})).toThrow();
+            expect(() => gameBoard.placeShip([0, 6], {length: length, vertical: true, testBoard: board})).toThrow();
+            expect(() => gameBoard.placeShip([1, 4], {length: length, vertical: true, testBoard: board})).toThrow();
+            expect(() => gameBoard.placeShip([4, 4], {length: length, testBoard: board})).toThrow();
         });
     });
 
--- src/app/gameboard.js
+++ src/app/gameboard.js
@@ -1,47 +1,50 @@
 export const GameBoard = () => {
     const board = Array.from({length: 10}, () => Array.from({length: 10}, () => 0));
 
-    const validateShipPlacement = (x, y, length, vertical) => {
+    const validateShipPlacement = (x, y, length, vertical, boardToUse) => {
         for(let i=0; i<length; i++){
             const row = vertical ? x+i : x;
             const col = vertical ? y : y+i;
 
-            if(row < 0 || row >= 10 || col < 0 || col >= 10 || board[row][col] === 1) throw new Error();
+            if(row < 0 || row >= 10 || col < 0 || col >= 10 || boardToUse[row][col] === 1) throw new Error();
             
             // Check for adjacent ships
             if(!vertical){
-                if(row-1 >= 0 && board[row-1][col] === 1) throw new Error();
-                if(row+1 < 10 && board[row+1][col] === 1) throw new Error();
-                if(y-1 >= 0 && board[row][y-1] === 1) throw new Error();
-                if(y+length < 10 && board[row][y+length] === 1) throw new Error(); 
+                if(row-1 >= 0 && boardToUse[row-1][col] === 1) throw new Error();
+                if(row+1 < 10 && boardToUse[row+1][col] === 1) throw new Error();
+                if(y-1 >= 0 && boardToUse[row][y-1] === 1) throw new Error();
+                if(y+length < 10 && boardToUse[row][y+length] === 1) throw new Error(); 
                 // check diagonal squares
-                if(y-1 >= 0 && row-1 >= 0 && row+1 < 10 && (board[row-1][y-1] || board[row+1][y-1])) throw new Error();
-                if(y+length < 10 && row-1 >= 0 && row+1 < 10 && (board[row-1][y+length] || board[row+1][y+length])) throw new Error();
+                if(y-1 >= 0 && row-1 >= 0 && row+1 < 10 && (boardToUse[row-1][y-1] || boardToUse[row+1][y-1])) throw new Error();
+                if(y+length < 10 && row-1 >= 0 && row+1 < 10 && (boardToUse[row-1][y+length] || boardToUse[row+1][y+length])) throw new Error();
             }
             else{
-                if(col-1 >= 0 && board[row][col - 1] === 1) throw new Error();
-                if(col+1 < 10 && board[row][col + 1] === 1) throw new Error();
-                if(x-1 >= 0 && board[x-1][col] === 1) throw new Error();
-                if(x+length < 10 && board[x+length][col] === 1) throw new Error();
+                if(col-1 >= 0 && boardToUse[row][col - 1] === 1) throw new Error();
+                if(col+1 < 10 && boardToUse[row][col + 1] === 1) throw new Error();
+                if(x-1 >= 0 && boardToUse[x-1][col] === 1) throw new Error();
+                if(x+length < 10 && boardToUse[x+length][col] === 1) throw new Error();
                 // check diagonal squares
-                if(x-1 >= 0 && col-1 >= 0 && col+1 < 10 && (board[x-1][col-1] || board[x-1][col+1])) throw new Error();
-                if(x+length < 10 && col-1 >= 0 && col+1 < 10 && (board[x+length][col-1] || board[x+length][col+1])) throw new Error();
+                if(x-1 >= 0 && col-1 >= 0 && col+1 < 10 && (boardToUse[x-1][col-1] || boardToUse[x-1][col+1])) throw new Error();
+                if(x+length < 10 && col-1 >= 0 && col+1 < 10 && (boardToUse[x+length][col-1] || boardToUse[x+length][col+1])) throw new Error();
             }
         }
     };
 
-    const placeShip = ([x, y], {length=1, vertical=false} = {}) => {
-        validateShipPlacement(x, y, length, vertical);
+    const placeShip = ([x, y], {length=1, vertical=false, testBoard=null} = {}) => {
+        // use the original board if testBoard is not passed
+        const boardToUse = testBoard || board;
+
+        validateShipPlacement(x, y, length, vertical, boardToUse);
 
         for(let i=0; i<length; i++){
             const row = vertical ? x+i : x;
             const col = vertical ? y : y+i;
 
-            board[row][col] = 1;
+            boardToUse[row][col] = 1;
         }
     };
 
-    const getBoard = () => board;
+    const getBoard = () => JSON.parse(JSON.stringify(board));
 
     return {placeShip, getBoard};
 };
