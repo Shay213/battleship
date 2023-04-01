a4ee544c58e7608467403a55e90037d6b495fdb1
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-04-01 13:20:45 +0200

    Create computer class that generates random moves

--- src/app/player.js
+++ src/app/player.js
@@ -16,3 +16,29 @@ export class Player{
         return this.enemyGameBoard.allSunk();
     }
 };
+
+export class ComputerPlayer extends Player{
+    constructor(enemyGameBoard){
+        super('Computer', enemyGameBoard);
+    }
+
+    playTurn(){
+        let cords = this.generateRandomCords();
+        while(!this.isValidMove(cords)){
+            cords = this.generateRandomCords();
+        }
+        this.enemyGameBoard.receiveAttack(cords);
+    }
+
+    generateRandomCords(){
+        const x = Math.floor(Math.random() * 10);
+        const y = Math.floor(Math.random() * 10);
+        return [x,y];
+    }
+
+    isValidMove(cords){
+        const board = this.enemyGameBoard.getBoard();
+        const [x,y] = cords;
+        return board[x][y] === 0;
+    }
+}
